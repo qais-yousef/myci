e227a9871e77dfe6b4782e08b08c3a72804bd058
Qais Yousef <qyousef@layalina.io>
2021-02-08 23:21:39 +0000

    setup_jenkins.sh: Setup to use https connection only
    To guarantee somewhat secure communication with the server, force https
    all the time. By default we create a selfsigned SSL certificate which is
    easier to setup. The generated jenkins_cert.pem must be distributed with
    myci cli to avoid python rejecting the self certified certificate.
    
    You can generate a proper certificate if security is a major concern in
    your setup.
    
    Documentation needs updating still.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- setup_jenkins.sh
+++ setup_jenkins.sh
@@ -12,3 +12,35 @@ docker volume create jenkins-docker-certs
 
 # Create volume jenkins_home
 #docker volume create jenkins-home
+
+# Generate a selfsigned certificate for https
+KEY_PASS_FILE=jenkins_keystore.pass
+KEY_FILE=jenkins_home/jenkins_keystore.jks
+PKCS12_FILE=jenkins_cert.p12
+PEM_FILE=jenkins_cert.pem
+san="$1"
+
+if [ ! -e $KEY_PASS_FILE ]; then
+    openssl rand -base64 32 > $KEY_PASS_FILE
+    chmod 600 key.pass
+fi
+
+if [ "x$san" != "x" ]; then
+	san="-ext san=$san"
+else
+	san=""
+fi
+
+keytool -genkey -keyalg RSA -alias selfsigned \
+	-keystore $KEY_FILE \
+	-storepass $(cat $KEY_PASS_FILE) \
+	$san \
+	-keysize 4096
+
+yes $(cat $KEY_PASS_FILE) | \
+	keytool -importkeystore -srckeystore $KEY_FILE \
+       -destkeystore $PKCS12_FILE \
+       -srcstoretype jks \
+       -deststoretype pkcs12
+
+openssl pkcs12 -in $PKCS12_FILE -out $PEM_FILE -passin "pass:$(cat $KEY_PASS_FILE)" -passout "pass:$(cat $KEY_PASS_FILE)"
--- start_jenkins.sh
+++ start_jenkins.sh
@@ -11,10 +11,12 @@ docker run \
   --env DOCKER_HOST=tcp://docker:2376 \
   --env DOCKER_CERT_PATH=/certs/client \
   --env DOCKER_TLS_VERIFY=1 \
-  --publish 8080:8080 \
-  --publish 8888:8888 \
-  --publish 50000:50000 \
+  --publish 8443:8443 \
   --volume /dev/bus/usb:/dev/bus/usb \
   --volume "$JENKINS_HOME":/var/jenkins_home \
   --volume jenkins-docker-certs:/certs/client:ro \
-  jenkins/jenkins:lts
+  jenkins/jenkins:lts \
+  --httpPort=-1 \
+  --httpsPort=8443 \
+  --httpsKeyStore=/var/jenkins_home/jenkins_keystore.jks \
+  --httpsKeyStorePassword=$(cat jenkins_keystore.pass)
