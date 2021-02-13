39d2b4d6685284ac249f88c31c1f5ec5067b2349
Qais Yousef <qyousef@layalina.io>
2021-02-13 22:10:11 +0000

    Add a script to regenerate the certificates
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

---
+++ regen_cert.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+set -x
+
+# Generate a selfsigned certificate for https
+KEY_PASS_FILE=jenkins_keystore.pass
+KEY_FILE=jenkins_home/jenkins_keystore.jks
+PKCS12_FILE=jenkins_cert.p12
+PEM_FILE=jenkins_cert.pem
+san="$1"
+
+rm $KEY_FILE $PKCS12_FILE $PEM_FILE
+
+if [ ! -e $KEY_PASS_FILE ]; then
+    openssl rand -base64 32 > $KEY_PASS_FILE
+    chmod 600 key.pass
+fi
+
+if [ "x$san" != "x" ]; then
+	san="-ext san=$san"
+else
+	san=""
+fi
+
+if [ ! -e $KEY_FILE ]; then
+	keytool -genkey -keyalg RSA -alias selfsigned \
+		-keystore $KEY_FILE \
+		-storepass $(cat $KEY_PASS_FILE) \
+		$san \
+		-keysize 4096
+fi
+
+if [ ! -e $PKCS12_FILE ]; then
+	yes $(cat $KEY_PASS_FILE) | \
+		keytool -importkeystore -srckeystore $KEY_FILE \
+	       -destkeystore $PKCS12_FILE \
+	       -srcstoretype jks \
+	       -deststoretype pkcs12
+fi
+
+if [ ! -e $PEM_FILE ]; then
+	openssl pkcs12 -in $PKCS12_FILE -out $PEM_FILE -passin "pass:$(cat $KEY_PASS_FILE)" -passout "pass:$(cat $KEY_PASS_FILE)"
+fi
