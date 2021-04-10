605ccb6c11960602e4e247568b77f127a4cedd88
Qais Yousef <qyousef@layalina.io>
2021-04-10 21:30:11 +0000

    vars: add new dev scripts
    Only implemented for Android targets for now.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

---
+++ vars/dev_pull.groovy
@@ -0,0 +1,16 @@
+def call(src, dst) {
+	switch (env.MYCI_NODE_TYPE) {
+	case "android":
+		if (env.IPADDRESS && env.PORT) {
+			sh """
+				adb -s ${IPADDRESS}:${PORT} pull \"${src}\" \"${dst}\"
+			"""
+		} else {
+			error "Missing IPADDRESS and/or PORT info"
+		}
+		break
+	default:
+		error "Not implemented for this node type: " + env.MYCI_NODE_TYPE
+		break
+	}
+}
