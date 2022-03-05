1f7b8444e7530e9d901124ca91cca37c0ff1001f
Qais Yousef <qyousef@layalina.io>
2022-03-05 15:51:07 +0000

    android_{doze, wakeup}_device: Use IPADDRESS nad PORT!
    We must use the IPADDRESS and PORT otherwise if there are multiple
    devices connected we'll end up with confusion or errors..
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- vars/android_doze_device.groovy
+++ vars/android_doze_device.groovy
@@ -1,13 +1,23 @@
 def call() {
-	sh """
-		state=`adb shell dumpsys power | grep -i wakefulness= | awk -F = '{print \$2}'`
+	switch (env.MYCI_NODE_TYPE) {
+	case "android":
+		if (env.IPADDRESS && env.PORT) {
+			sh '''
+				state=`adb -s ${IPADDRESS}:${PORT} shell dumpsys power | grep -i wakefulness= | awk -F = '{print \$2}'`
 
-		if [ "\$state" == "Awake" ]; then
-			adb shell input keyevent 26
-		fi
+				if [ "\$state" == "Awake" ]; then
+					adb -s ${IPADDRESS}:${PORT} shell input keyevent 26
+				fi
 
-		if [ -e .myci_android_display_timeout ]; then
-			adb shell settings put system screen_off_timeout \$(cat .myci_android_display_timeout)
-		fi
-	"""
+				if [ -e .myci_android_display_timeout ]; then
+					adb -s ${IPADDRESS}:${PORT} shell settings put system screen_off_timeout \$(cat .myci_android_display_timeout)
+				fi
+			'''
+		} else {
+			error "Missing IPADDRESS and/or PORT info"
+		}
+		break
+	default:
+		break
+	}
 }
--- vars/android_wakeup_device.groovy
+++ vars/android_wakeup_device.groovy
@@ -1,19 +1,29 @@
 def call() {
-	sh """
-		state=`adb shell dumpsys power | grep -i wakefulness= | awk -F = '{print \$2}'`
+	switch (env.MYCI_NODE_TYPE) {
+	case "android":
+		if (env.IPADDRESS && env.PORT) {
+			sh '''
+				state=`adb -s ${IPADDRESS}:${PORT} shell dumpsys power | grep -i wakefulness= | awk -F = '{print \$2}'`
 
-		if [ "\$state" != "Awake" ]; then
-			adb shell input keyevent 26
-			sleep 1
-			adb shell input touchscreen swipe 200 800 200 100
-		fi
+				if [ "\$state" != "Awake" ]; then
+					adb -s ${IPADDRESS}:${PORT} shell input keyevent 26
+					sleep 1
+					adb -s ${IPADDRESS}:${PORT} shell input touchscreen swipe 200 800 200 100
+				fi
 
-		# Set timeout to 30 mins if not already set to that
-		timeout=`adb shell settings get system screen_off_timeout`
+				# Set timeout to 30 mins if not already set to that
+				timeout=`adb -s ${IPADDRESS}:${PORT} shell settings get system screen_off_timeout`
 
-		if [ "\$timeout" != "1800000" ]; then
-			adb shell settings put system screen_off_timeout 1800000
-			echo \$timeout > .myci_android_display_timeout
-		fi
-	"""
+				if [ "\$timeout" != "1800000" ]; then
+					adb -s ${IPADDRESS}:${PORT} shell settings put system screen_off_timeout 1800000
+					echo \$timeout > .myci_android_display_timeout
+				fi
+			'''
+		} else {
+			error "Missing IPADDRESS and/or PORT info"
+		}
+		break
+	default:
+		break
+	}
 }
