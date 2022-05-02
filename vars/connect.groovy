064f79f9bb7b16fa5709aa6772bcb315f7d45fbd
Qais Yousef <qyousef@layalina.io>
2022-05-02 12:08:37 +0100

    connect: Be more resilient to failures to connect to adb device
    For some reason we could timeout or the device could connect in offline
    more. It seems the devices having its screen off and being in deep sleep
    plays a part in this instability. Maybe we need to improve
    a configuration option to ensure adb wifi is more responsive?
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- vars/connect.groovy
+++ vars/connect.groovy
@@ -3,7 +3,21 @@ def call() {
 	case "android":
 		if (env.IPADDRESS && env.PORT) {
 			sh '''
-				adb connect ${IPADDRESS}:${PORT}
+				retry=5
+				for i in \$(seq \$retry)
+				do
+					adb connect ${IPADDRESS}:${PORT}
+
+					status=`adb devices | grep ${IPADDRESS} | awk '{print $2}'`
+
+					if [ "x\$status" == "xdevice" ]; then
+						break
+					fi
+
+					adb disconnect ${IPADDRESS}:${PORT} || true
+
+					sleep 3
+				done
 				adb -s ${IPADDRESS}:${PORT} root || true
 				adb -s ${IPADDRESS}:${PORT} remount || true
 				adb -s ${IPADDRESS}:${PORT} shell "echo temp > /sys/power/wake_lock" || true
