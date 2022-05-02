8e8486716a693116a77633294156815bf2bbb865
Qais Yousef <qyousef@layalina.io>
2022-05-02 18:21:56 +0100

    Revert "disconnect: Keep adb connection alive"
    This reverts commit 8a3aff3781e62b151a9285ea0f47bfaad7a762be.
    
    This didn't help with adb instability with a NODE is communicating with
    multiple Android DUTs.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- vars/disconnect.groovy
+++ vars/disconnect.groovy
@@ -4,9 +4,7 @@ def call() {
 		if (env.IPADDRESS && env.PORT) {
 			sh '''
 				adb -s ${IPADDRESS}:${PORT} shell "echo temp > /sys/power/wake_unlock" || true
-
-				# No need to disconnect to save hassle connecting again when we start a new test
-				# adb disconnect ${IPADDRESS}:${PORT}
+				adb disconnect ${IPADDRESS}:${PORT}
 			'''
 		} else {
 			error "Missing IPADDRESS and/or PORT info"
