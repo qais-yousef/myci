ecfa08551b0ac00c1020f57fca3bbeda50121aee
Qais Yousef <qyousef@layalina.io>
2022-05-05 22:43:22 +0100

    disconnect: Kill adb server daemon of no devices are attached
    This forces a fresh server to start on next connection. There are weird
    adb connection stability issues that might be attributed to long running
    server daemon going funny. This is an attempt to improve the stability
    by regularly killing the daemon when no devices are attached so that the
    daemon is refreshed on next connection.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- vars/disconnect.groovy
+++ vars/disconnect.groovy
@@ -5,6 +5,15 @@ def call() {
 			sh '''
 				adb -s ${IPADDRESS}:${PORT} shell "echo temp > /sys/power/wake_unlock" || true
 				adb disconnect ${IPADDRESS}:${PORT}
+
+				# Force a fresh start-server on next start if
+				# no devices are connected.
+				# After a while some weird connection issues
+				# occur for a long running server daemon..
+				status=`adb devices | grep -v 'List of devices attached' | awk '{print $2}'`
+				if [ "x\$status" == "x" ]; then
+					adb kill-server
+				fi
 			'''
 		} else {
 			error "Missing IPADDRESS and/or PORT info"
